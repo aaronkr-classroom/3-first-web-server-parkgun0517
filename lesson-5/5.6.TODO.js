1cb3c7eabfa0d34d67fb0da347bff1a6fec59bfa
parkgun0517 <163221048+parkgun0517@users.noreply.github.com>
2024-03-27 01:48:24 +0000

    finished

--- lesson-5/5.6.TODO.js
+++ lesson-5/5.6.TODO.js
@@ -8,15 +8,16 @@
 
 // 요청에 따른 라우트의 매핑 정의
 const routeRespMap = {
-    "/": "<h1>Hello Everybody</h1>",
+    "/": "<h1>Hello Everybody!</h1>",
     "/contact": "<h1>Contact Page</h1>",
     "/about": "<h1>About Page</h1>",
+    "/hello": "<h1>Hello Page</h1>",
     "/error": "<h1>Error Page</h1>",
     "/info": "<h1>Info Page</h1>",
 };
 
 // listing5.5.js에서 (p. 96)
-const port = 3000,
+const port = 3001,
     http = require('http'),
     httpStatus = require('http-status-codes'),
     app = http.createServer(),
@@ -25,12 +26,12 @@ const port = 3000,
     };
 
 // 요청 라우트가 정의된 맵에 있는지 체크
-app.on("request",(req, res) => {
+app.on("request", (req, res) => {
     res.writeHead(httpStatus.OK, {
         "Content-Type": "text/html"
     });
 
-    if (routeRespMap[req.url]){
+    if(routeRespMap[req.url]) {
         res.end(routeRespMap[req.url]);
     } else {
         setTimeout(() => {
@@ -38,6 +39,7 @@ app.on("request",(req, res) => {
         }, 2000);
     }
 });
+
 // <<< 나머진 서버 코드 입력 하십시오 >>>
 
 // listing5.7.js에서 (p. 98)
@@ -45,7 +47,11 @@ app.on("request",(req, res) => {
 // setTimeout(() => res.end(routeRespMap[req.url]), 2000); 
 // list5.6 res.end() 코드를 이 줄로 대체하여 서버 처리 지연을 시뮬레이트하십시오.
 
-if(process.env.NODE_ENV !== "test")
+if(process.env.NODE_ENV !== "test") 
     app.listen(port);
 
-console.log(`Sever at: http://localhost:${port}`);
+console.log(`Server at: http://localhost:${port}`);
+
+
+
+module.exports = app;
